dfee64f09ddd0adce08b7ca7e39ab3173198eb2b
Mael FOSSO <fosso.mael.elvis@gmail.com>
2021-01-07 02:51:21 +0100

    Main activities xml simply done

---
+++ app/src/main/java/com/maelfosso/bleck/iotremotecontrol/Settings.kt
@@ -0,0 +1,20 @@
+package com.maelfosso.bleck.iotremotecontrol
+
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+
+class Settings : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+}
