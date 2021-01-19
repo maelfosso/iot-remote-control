8013564b3da1cc25110b4b5b88f642393062bdf1
Mael FOSSO <fosso.mael.elvis@gmail.com>
2021-01-19 07:26:00 +0100

    files (activity, fragment) created for bluetooth connection

---
+++ app/src/main/java/com/maelfosso/bleck/iotremotecontrol/BluetoothConnectionActivity.kt
@@ -0,0 +1,20 @@
+package com.maelfosso.bleck.iotremotecontrol
+
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+
+class BluetoothConnectionActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bluetooth_connection)
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+}
